fe5c94e5269889facbe21f68425d463223445ad4
Jesus Villajos <jesus.villajos@gmail.com>
2020-09-26 21:15:14 +0200

    add authenciation artifacts

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,20 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/models/User';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  currentUserValue: any;
+  private currentUserSubject: BehaviorSubject<User>;
+  private isLoginSubject: BehaviorSubject<boolean>;
+  public currentUser: Observable<User>;
+  public isLogin: Observable<boolean>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+      this.isLoginSubject = new BehaviorSubject<boolean>(this.isValidUser());
+      this.currentUser = this.currentUserSubject.asObservable();
+      this.isLogin = this.isLoginSubject.asObservable();
+  }
 
+  public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+  }
+
+  isValidUser(): boolean {
+    return this.currentUser != null;
+  }
 
   register(registerUserDto: any) {
-    const headers = new HttpHeaders()
-      .append('Content-Type', 'application/json')
-      .append('Access-Control-Allow-Origin', '*');
+    return this.http.post(`${environment.API_URL}/authentication/register`, registerUserDto);
+  }
 
-    return this.http.post(`http://localhost:5000/api/authentication/register`, registerUserDto, { headers });
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.API_URL}/authentication/login`, { username, password })
+        .pipe(map(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+            this.isLoginSubject.next(true);
+            return user;
+        }));
+  }
+
+  logout() {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+    this.isLoginSubject.next(false);
   }
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/User';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
+  currentUser: User;
+  isLogin: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService) {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit(): void {
+    this.authService.currentUser.subscribe(x => this.currentUser = x);
+    this.authService.isLogin.subscribe(x => this.isLogin = x);
   }
 
 }
